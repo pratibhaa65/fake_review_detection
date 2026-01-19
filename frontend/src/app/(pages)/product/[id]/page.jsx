2fabfc04be616abbe2da3b6de8a9ab7591f55ae0
prajjwolcodes <shresthaprajjwol4@gmail.com>
2026-01-19 19:34:23 +0545

    fixed routes

--- frontend/src/app/(pages)/product/[id]/page.jsx
+++ frontend/src/app/(pages)/product/[id]/page.jsx
@@ -3,12 +3,9 @@ import ReviewForm from "../reviews";
 const Page = async ({ params }) => {
   const { id } = await params;
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products`,
-    {
-      cache: "no-store",
-    },
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`, {
+    cache: "no-store",
+  });
   const data = await res.json();
 
   const product = data.products.find((product) => product.id.toString() === id);
