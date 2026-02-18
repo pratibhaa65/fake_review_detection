bb5c8ca0e8ab00f2c296b86ac40d584ba55e5f6e
Pratibha Sharma <pratibhaa625@gmail.com>
2026-02-18 21:08:05 +0545

    env

--- backend_service/database/db.py
+++ backend_service/database/db.py
@@ -1,7 +1,8 @@
+from multiprocessing import process
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:frds@localhost:5432/review_db"
+DATABASE_URL = process.env.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
